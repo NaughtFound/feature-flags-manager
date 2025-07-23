3a7ef8ab80895f0e6be2e92d9fd4493b21c882ad
Mohammad Ghazanfari <mgh.5225@gmail.com>
2025-07-23 10:20:47 +0330

    minor bug fixed

--- src/flags/flags.module.ts
+++ src/flags/flags.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { FlagsController } from './flags.controller';
 import { FlagsService } from './flags.service';
+import { Flag } from '@app/db/entities/flag.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Flag])],
   controllers: [FlagsController],
-  providers: [FlagsService]
+  providers: [FlagsService],
 })
 export class FlagsModule {}

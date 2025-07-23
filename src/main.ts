cf49858ad903c67b0bc387c12064f4ec4fb1eb87
Mohammad Ghazanfari <mgh.5225@gmail.com>
2025-07-23 19:21:01 +0330

    ThrowExceptionFilter added

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import apiConfig from './api/config';
+import { ThrowExceptionFilter } from './utils/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
+  app.useGlobalFilters(new ThrowExceptionFilter());
 
   const configService = app.get<ConfigService>(ConfigService);
   const data = apiConfig(configService);
---
+++ src/utils/exception.filter.ts
@@ -0,0 +1,30 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class ThrowExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status =
+      exception instanceof HttpException ? exception.getStatus() : 500;
+
+    let message: any = 'Internal server error';
+
+    if (exception instanceof HttpException) message = exception.getResponse();
+    if (exception instanceof Error) message = exception.message;
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message,
+    });
+  }
+}

48f5a0774987711a47b9ebc3919e488d00fc7f7a
Mohammad Ghazanfari <mgh.5225@gmail.com>
2025-07-23 11:07:19 +0330

    listFlags added with a test

--- src/flags/flags.service.spec.ts
+++ src/flags/flags.service.spec.ts
@@ -1,18 +1,86 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FlagsService } from './flags.service';
+import { Flag, FlagRepo } from '@app/db/entities/flag.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { topologicalSort } from '@app/utils';
+
+jest.mock('@app/utils', () => ({
+  topologicalSort: jest.fn(),
+}));
 
 describe('FlagsService', () => {
   let service: FlagsService;
+  let flagRepo: jest.Mocked<FlagRepo>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FlagsService],
+      providers: [
+        FlagsService,
+        {
+          provide: getRepositoryToken(Flag),
+          useValue: {
+            find: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<FlagsService>(FlagsService);
+    flagRepo = module.get(getRepositoryToken(Flag));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return flags in topological order', async () => {
+    const flagA: Flag = {
+      id: 1,
+      label: 'A',
+      isActive: true,
+      dependencies: [],
+    };
+
+    const flagB: Flag = {
+      id: 2,
+      label: 'B',
+      isActive: true,
+      dependencies: [flagA],
+    };
+
+    const flagC: Flag = {
+      id: 3,
+      label: 'C',
+      isActive: true,
+      dependencies: [flagA, flagB],
+    };
+
+    const flagD: Flag = {
+      id: 4,
+      label: 'D',
+      isActive: true,
+      dependencies: [flagA],
+    };
+
+    const flags = [flagA, flagB, flagC, flagD];
+
+    flagRepo.find.mockResolvedValue(flags);
+
+    (topologicalSort as jest.Mock).mockReturnValue([0, 1, 3, 2]);
+
+    const result = await service.listFlags();
+
+    expect(flagRepo.find).toHaveBeenCalledWith({
+      relations: { dependencies: true },
+    });
+
+    expect(topologicalSort).toHaveBeenCalledWith([
+      [0, 1, 1, 1],
+      [0, 0, 1, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ]);
+
+    expect(result).toEqual([flagA, flagB, flagD, flagC]);
   });
 });
--- src/flags/flags.service.ts
+++ src/flags/flags.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Flag, FlagRepo } from '@app/db/entities/flag.entity';
+import { topologicalSort } from '@app/utils';
 
 @Injectable()
 export class FlagsService {
@@ -8,4 +9,37 @@ export class FlagsService {
     @InjectRepository(Flag)
     private flagRepo: FlagRepo,
   ) {}
+
+  async listFlags() {
+    const flags = await this.flagRepo.find({
+      relations: {
+        dependencies: true,
+      },
+    });
+
+    const n = flags.length;
+
+    const adjMatrix: number[][] = Array.from({ length: n }, () =>
+      Array(n).fill(0),
+    );
+
+    const flagIndex = new Map<number, number>();
+
+    for (let i = 0; i < n; i++) {
+      const u = flags[i];
+      flagIndex.set(u.id, i);
+    }
+
+    for (let i = 0; i < n; i++) {
+      const u = flags[i];
+      for (const parent of u.dependencies) {
+        const p = flagIndex.get(parent.id)!;
+        adjMatrix[p][i] = 1;
+      }
+    }
+
+    const order = topologicalSort(adjMatrix);
+
+    return order.map((index) => flags[index]);
+  }
 }
